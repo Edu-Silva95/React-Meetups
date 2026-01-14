f0b0a4611d97e099ff10b5d1c9a0833e8c7fbdf9
Eduardo Silva <joaomalta4@hotmail.com>
2026-01-14 19:44:47 +0000

    Another update

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -6,6 +6,7 @@ function MeetupDetails(props) {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{props.meetupData.title}</title>
         <meta name="description" content={props.meetupData.description} />
       </Head>
