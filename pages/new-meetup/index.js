2215e75f264887b6a5835dcb138fd97d44b12623
Eduardo Silva <joaomalta4@hotmail.com>
2026-01-14 19:41:15 +0000

    Still trying to fix the same bug

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -21,6 +21,7 @@ function NewMeetupPage() {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Add a New Meetup</title>
         <meta
           name="description"
